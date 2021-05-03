7d62589ffedd2a7736be01d7baa41049e3785743
nurhamidan <agungnurhamidan@gmail.com>
2021-05-03 18:06:59 +0700

    Selesai melakukan desain UI

---
+++ app/src/main/java/com/nurhamidan/tugas2monitoriapplication/BiodataActivity.java
@@ -0,0 +1,14 @@
+package com.nurhamidan.tugas2monitoriapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BiodataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_biodata);
+    }
+}
---
+++ app/src/main/java/com/nurhamidan/tugas2monitoriapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.nurhamidan.tugas2monitoriapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/*
+    NIM : 10118017
+    Nama : Agung Nurhamidan
+    Kelas : IF-1
+    Tanggal Pengerjaan : 3 Mei 2021
+    Deskripsi Pekerjaan : Untuk memenuhi tugas 3 membuat aplikasi Monitory
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
